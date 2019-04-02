37687548ade198b48ec31cb7bc5ca92bed6fb7f8
Hiroki Tanaka <rokitaniplus1@gmail.com>
2019-04-02 20:39:18 +0900

    rename the files named "*jp*"

--- DataMake.sh
+++ DataMake.sh
@@ -2,25 +2,18 @@
 cd GPSRDATA
 if [ $1 = "GPSR" ]; then
 echo "GPSR"
-rm GPSRSentenceJP.csv
+rm GPSRSentence.csv
 python GPSRsentence_generator2015enMakelabeldata.py || exit
 echo "python実行できました."
 touch ../OriginDataset/traindata.csv
 touch ../OriginDataset/testdata.csv
 touch ../OriginDataset/devdata.csv
-#sed -e '10000,$d' GPSRSentenceJP.csv > traindata.csv
-sed -e '10000,$d' GPSRSentenceJP.csv > ../OriginDataset/traindata.csv
-#sed -n '10001,12001p' GPSRSentenceJP.csv > testdata.csv
-sed -n '10001,12001p' GPSRSentenceJP.csv > ../OriginDataset/testdata.csv
-#sed -n '12002,14002p' GPSRSentenceJP.csv > devdata.csv
-sed -n '12002,14002p' GPSRSentenceJP.csv > ../OriginDataset/devdata.csv
-#cp  testdata.csv ../OriginDataset
-#cp  traindata.csv ../OriginDataset
-#cp  devdata.csv ../OriginDataset
+sed -e '10000,$d' GPSRSentence.csv > ../OriginDataset/traindata.csv
+sed -n '10001,12001p' GPSRSentence.csv > ../OriginDataset/testdata.csv
+sed -n '12002,14002p' GPSRSentence.csv > ../OriginDataset/devdata.csv
 cd ../OriginDataset
 gsed -e '1idata,labeldata' -i testdata.csv
 gsed -e '1idata,labeldata' -i devdata.csv
 else
-  echo "GPSRSentenceJPを実行しません."
+  echo "GPSRSentenceを実行しません."
 fi
-
