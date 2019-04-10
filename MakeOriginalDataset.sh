8e440b3a34e28d34a22a1d928702eb2bfecd366e
Hiroki Tanaka <rokitaniplus1@gmail.com>
2019-04-10 22:52:03 +0900

    Update MakeOriginalDataset.sh

--- MakeOriginalDataset.sh
+++ MakeOriginalDataset.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 cd GPSRDATA
-if [ $1 = "GPSR" ]; then
-echo "GPSR"
 rm GPSRSentence.csv
 python GPSRsentence_generator2015enMakelabeldata.py || exit
 echo "python実行できました."
-touch ../OriginDataset/traindata.csv
-touch ../OriginDataset/testdata.csv
-touch ../OriginDataset/devdata.csv
-sed -e '10000,$d' GPSRSentence.csv > ../OriginDataset/traindata.csv
-sed -n '10001,12001p' GPSRSentence.csv > ../OriginDataset/testdata.csv
-sed -n '12002,14002p' GPSRSentence.csv > ../OriginDataset/devdata.csv
-cd ../OriginDataset
+touch ../OriginalDataset/traindata.csv
+touch ../OriginalDataset/testdata.csv
+touch ../OriginalDataset/devdata.csv
+sed -e '10000,$d' GPSRSentence.csv > ../OriginalDataset/traindata.csv
+sed -n '10001,12001p' GPSRSentence.csv > ../OriginalDataset/testdata.csv
+sed -n '12002,14002p' GPSRSentence.csv > ../OriginalDataset/devdata.csv
+cd ../OriginalDataset
 gsed -e '1idata,labeldata' -i testdata.csv
 gsed -e '1idata,labeldata' -i devdata.csv
-else
-  echo "GPSRSentenceを実行しません."
-fi
