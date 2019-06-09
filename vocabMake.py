09bce6f0ee6f6f58811bcfb0e7a11f2cd9f087a0
emlab <rokitaniplus1@gmail.com>
2019-06-09 23:53:15 +0900

    edit vocabMake.py

--- vocabMake.py
+++ vocabMake.py
@@ -1,17 +1,14 @@
-import matplotlib.pylab as plt
 import numpy as np
-import datetime
 import pickle
 import os
-import glob
 import sys
 import math
 import pandas as pd
 
-def Readdata(filename,Inputhead):
+def Readdata(filename,inputhead):
     df = pd.read_csv(filename,encoding="utf-8")
-    datas = df[Inputhead]
-    label_datas = df["Labeldata"]
+    datas = df[inputhead]
+    label_datas = df["labeldata"]
 
     def Append(datas):
         textdatas = []
@@ -34,42 +31,25 @@ def Readdata(filename,Inputhead):
 
     return datas, label_datas
 
+def SentenceLength(filedatalist):
+    max_sentence_length = 0
+    for sentence in filedatalist:
+        if max_sentence_length < len(sentence):
+            max_sentence_length = len(sentence)
+    return max_sentence_length
 
-def OnehotMakedata(filedatalist,picklefilename,max_sentence_length = 0):
-    for word in filedatalist:
-        if max_sentence_length < len(word):
-            max_sentence_length = len(word)
-    sentence_length = max_sentence_length
 
-    print(sentence_length)
+
+def OnehotMakedata(filedatalist,picklefilename):
     c_f = {}  
     depth = 250  
-
-    for data in filedatalist:
-        for word in data:
-            if c_f.get(word) is None:
-                c_f[word] = 0
-            c_f[word] += 1
+    for sentence in filedatalist:
+        for symbol in sentence:
+            if c_f.get(symbol) is None:
+                c_f[symbol] = 0
+            c_f[symbol] += 1
     c_i = {c : e for e, (c, f) in enumerate(sorted(c_f.items(), key=lambda x: x[1] * -1)[:depth])}
     open(picklefilename, "wb").write(pickle.dumps(c_i))
-
-    onehot_vector = []
-    Next_onehot_vector = []
-
-    for i,data in enumerate(filedatalist):
-        xd = [[0.] * len(c_i) for _ in range(sentence_length)]
-        Next_target =  [[0.] * len(c_i) for _ in range(sentence_length)]
-        for l,word in enumerate(data):
-            xd[l][c_i[word]] = 1.
-            if l > 0 :
-                Next_target[l - 1][c_i[word]] = 1.
-            elif l >= len(data) :
-                Next_target[l][c_i["<EOS>"]] = 1.
-
-        onehot_vector.append(np.array(list(xd)))
-        Next_onehot_vector.append(np.array(list(Next_target)))
-
-    return np.array(onehot_vector),np.array(Next_onehot_vector)
 
 def Makevocab(picklefilename,outputfilename):
     c_i = pickle.load(open(picklefilename, "rb"))
@@ -81,15 +61,12 @@ def Makevocab(picklefilename,outputfilename):
             f.write(word+"\n")
 
 if __name__ == '__main__':
-    traindata_filename = "/Users/pro11/Desktop/yota/Master_thesis/source/NoiseMakeData/Dataset/"+sys.argv[1]
-    train_V_picklename = "Vocabulary.pkl"
-    print("data Download")
-    train_data, label_datas = Readdata(traindata_filename,sys.argv[2])
-    if sys.argv[2] == "Input":
-        label_V_picklename = "Vocabularytrain.pkl"
-        train_ohl_datas, Next_ohl_target = OnehotMakedata(label_datas, label_V_picklename)
-        Makevocab(label_V_picklename,"./Dataset/Vocab.out")
-
-    print("Onehot Vector")
-    train_oh_datas, Next_oh_target = OnehotMakedata(train_data, train_V_picklename)
-    Makevocab(train_V_picklename,"./Dataset/Vocab"+sys.argv[2]+".in")
+    data_filename = "./OriginalDataset/"+sys.argv[1]
+    datas, label_datas = Readdata(data_filename,sys.argv[2])
+    print("sentence_length:"+str(SentenceLength(data_filename)))
+    picklename = "Vocabulary.pkl"
+    OnehotMakedata(datas, picklename)
+    if sys.argv[2] == "label":
+        Makevocab(picklename,"./Dataset/Vocab.out")
+    else:
+        Makevocab(picklename,"./Dataset/Vocab"+sys.argv[2]+".in")
