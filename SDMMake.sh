e6abed4ad267156ad464cb22a047f7117aa27568
emlab <rokitaniplus1@gmail.com>
2019-06-09 23:52:03 +0900

    separate SDMMake.sh and MakeVocab.sh

--- SDMMake.sh
+++ SDMMake.sh
@@ -1,11 +1,6 @@
 
 echo "SDM.py"
-python SDM.py testdata.csv || echo "SDM.py testdata.csv failed"
+#python SDM.py testdata.csv || echo "SDM.py testdata.csv failed"
 python SDM.py traindata.csv || echo "SDM.py traindata.csv failed"
-python SDM.py devdata.csv || echo "SDM.py testdata.csv failed"
+#python SDM.py devdata.csv || echo "SDM.py testdata.csv failed"
 
-echo "start vocabMake"
-echo "vocabMake.py"
-python vocabMake.py traindata.csv Input
-python vocabMake.py traindata.csv phonemelabel
-python vocabMake.py traindata.csv Noisylabel
