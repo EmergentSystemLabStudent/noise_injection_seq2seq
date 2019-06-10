7aec1e14fc89112ebca9dd2ef0f8a2aeaa7ecd49
emlab <rokitaniplus1@gmail.com>
2019-06-11 03:21:48 +0900

    tidy up

--- Dataset/mv.sh
+++
@@ -1,7 +0,0 @@
-
-
-#mv PI_0.3PS_0.3PD_0.3PS_S0.01$1traindata.in PI_0.3PS_0.3PD_0.3PS_S0.1$1traindata.in
-#mv PI_0.3PS_0.3PD_0.3PS_S0.01$1testdata.in PI_0.3PS_0.3PD_0.3PS_S0.1$1testdata.in
-#mv PI_0.3PS_0.3PD_0.3PS_S0.01$1devdata.in PI_0.3PS_0.3PD_0.3PS_S0.1$1devdata.in
-
-mv PI_0.3PS_0.3PD_0.3PS_S0.01label$1.out PI_0.3PS_0.3PD_0.3PS_S0.1label$1.out
