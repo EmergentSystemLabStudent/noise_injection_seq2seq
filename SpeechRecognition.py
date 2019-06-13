aae28b974c28f3b574d041650cb5658ff634a49b
emlab <rokitaniplus1@gmail.com>
2019-06-13 19:43:58 +0900

    speech recognition

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -7,31 +7,26 @@ import glob
 import re
 
 def SpeechRecognition(filename):
+
     r = speech_recognition.Recognizer()
     with speech_recognition.AudioFile(filename) as source:
         audio = r.record(source)
 
     google_result=r.recognize_google(audio, show_all=True)
-    sphinx_result=r.recognize_sphinx(audio, show_all=True)
-
 
     units = {'0':'zero','1':'one','2':'two','3':'three','4':'four','5':'five','6':'six','7':'seven','8':'eight','9':'nine'}
 
-    print("Google_result:",google_result["alternative"][0]['transcript'])
-    print("Sphinx_result:",sphinx_result.hyp().hypstr)
-
+    #print("Google_result:",google_result["alternative"][0]['transcript'])
+    
     google_phoneme = []
-
     for word in (google_result["alternative"][0]['transcript']).split():
-            print(word)
-            #word =format_text(word)
             #print(word)
             ph = pronouncing.phones_for_word(str(word))
             if not ph:
                 word =re.sub(re.compile("[!-/:-@[-`{-~]"), '', word)
-                print("--------------------Word :", word, "--------------------")
+                #print("--------------------Word :", word, "--------------------")
                 for phoneme in str(word):
-                    print(phoneme.isdecimal())
+                    #print(phoneme.isdecimal())
                     if phoneme.isdecimal():
                         phoneme = units[phoneme]
                     elif phoneme.find(".") > -1:
@@ -43,13 +38,17 @@ def SpeechRecognition(filename):
                         exit()
                     else :
                         ph = ph[0].split()
-                        print(ph)
+                        #print(ph)
                     google_phoneme.extend(ph)
             else:
                 ph = ph[0].split()
-                print(ph)
+                #print(ph)
                 google_phoneme.extend(ph)
+    #print("Google_phoneme_result:",google_phoneme)
 
+    """
+    sphinx_result=r.recognize_sphinx(audio, show_all=True)
+    print("Sphinx_result:",sphinx_result.hyp().hypstr)                
     sphinx_phoneme = []
     for word in (sphinx_result.hyp().hypstr).split():
             print(word)
@@ -74,42 +73,46 @@ def SpeechRecognition(filename):
                 ph = ph[0].split()
                 print(ph)
                 sphinx_phoneme.extend(ph)
-
-    print("Google_phneme_result:",google_phoneme)
     print("Sphinx_phneme_result:",sphinx_phoneme)
-'''
-    frame =pd.DataFrame([[filename,google_result["alternative"][0]['transcript']," ".join(google_phoneme),sphinx_result.hyp().hypstr," ".join(sphinx_phoneme)]],columns=["Data","google_result","google_phoneme","sphinx_result","sphinx_phoneme"])
+"""
 
-    path =dir+"google_Wordresultdata.csv"
-    df = frame["google_result"].to_csv(path,index=False,mode='a')
+    
+    result = [filename,google_result["alternative"][0]['transcript']," ".join(google_phoneme)]
+    return result
 
-    path =dir+"google_phonemeresultdata.csv"
-    df = frame["google_phoneme"].to_csv(path,index=False,mode='a')
+    #path =dir+"google_Wordresultdata.csv"
+    #df = frame["google_result"].to_csv(path,index=False,mode='a')
 
-    path =dir+"sphinx_wordresultdata.csv"
-    df = frame["sphinx_result"].to_csv(path,index=False,mode='a')
+    #path =dir+"google_phonemeresultdata.csv"
+    #df = frame["google_phoneme"].to_csv(path,index=False,mode='a')
 
-    path =dir+"sphinx_phonemeresultdata.csv"
-    df = frame["sphinx_phoneme"].to_csv(path,index=False,mode='a')
+    #path =dir+"sphinx_wordresultdata.csv"
+    #df = frame["sphinx_result"].to_csv(path,index=False,mode='a')
 
-    path =dir+"checkdata.csv"
-    frame =pd.DataFrame([[filename,google_result["alternative"][0]['transcript'],sphinx_result.hyp().hypstr]],columns=["Data","google_result","sphinx_result"])
-    frame.to_csv(path, index=False, mode='a',header=False)
+    #path =dir+"sphinx_phonemeresultdata.csv"
+    #df = frame["sphinx_phoneme"].to_csv(path,index=False,mode='a')
+
+    #path =dir+"checkdata.csv"
+    #frame =pd.DataFrame([[filename,google_result["alternative"][0]['transcript'],sphinx_result.hyp().hypstr]],columns=["Data","google_result","sphinx_result"])
+    #frame.to_csv(path, index=False, mode='a',header=False)
 
 
-    path =dir+"RecognitionResult.csv"
-    if not os.path.exists(path):
-        df = frame.to_csv(path,index=False,mode='w')
-    else:
-        df = frame.to_csv(path, index=False, mode='a',header=False)
-'''
+    #path =dir+"RecognitionResult.csv"
+    #if not os.path.exists(path):
+    #    df = frame.to_csv(path,index=False,mode='w')
+    #else:
+    #    df = frame.to_csv(path, index=False, mode='a',header=False)
+
 
 if __name__ == '__main__':
-    dir = "./utterance_data/speaker_K/"
+    dir = sys.argv[1]
+    
     fitsf=sorted(glob.glob(dir+"*.wav"))
+    results = []
     for filename in fitsf:
-        SpeechRecogniton(filename)
-
-
-
+        print(filename)
+        result=SpeechRecognition(filename)
+        results.append(result)
+    df = pd.DataFrame(results,columns=["filename","google_word","google_phoneme"])
+    df.to_csv(dir+"SpeechRecognitionResults.csv",index=False)
 
