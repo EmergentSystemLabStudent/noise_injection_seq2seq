3e1a9e280faa257514bbb2bb42183f1ed78e7ebc
emlab <rokitaniplus1@gmail.com>
2019-06-19 14:14:16 +0900

    MakeSDM.sh

--- SDM/MakeSDM.sh
+++ SDM/MakeSDM.sh
@@ -1,6 +1,28 @@
 
 echo "SDM.py"
-#python SDM.py testdata.csv || echo "SDM.py testdata.csv failed"
-python SDM.py ./Dataset/ ./OriginalDataset/ traindata.csv || echo "SDM.py traindata.csv failed"
-#python SDM.py devdata.csv || echo "SDM.py testdata.csv failed"
+
+python SDM.py ./Dataset/ ./OriginalDataset/ traindata.csv 0.0
+python SDM.py ./Dataset/ ./OriginalDataset/ traindata.csv 0.05
+python SDM.py ./Dataset/ ./OriginalDataset/ traindata.csv 0.1
+python SDM.py ./Dataset/ ./OriginalDataset/ traindata.csv 0.15
+python SDM.py ./Dataset/ ./OriginalDataset/ traindata.csv 0.2
+python SDM.py ./Dataset/ ./OriginalDataset/ traindata.csv 0.25
+python SDM.py ./Dataset/ ./OriginalDataset/ traindata.csv 0.3
+
+python SDM.py ./Dataset/ ./OriginalDataset/ devdata.csv 0.0
+python SDM.py ./Dataset/ ./OriginalDataset/ devdata.csv 0.05
+python SDM.py ./Dataset/ ./OriginalDataset/ devdata.csv 0.1
+python SDM.py ./Dataset/ ./OriginalDataset/ devdata.csv 0.15
+python SDM.py ./Dataset/ ./OriginalDataset/ devdata.csv 0.2
+python SDM.py ./Dataset/ ./OriginalDataset/ devdata.csv 0.25
+python SDM.py ./Dataset/ ./OriginalDataset/ devdata.csv 0.3
+
+python SDM.py ./Dataset/ ./OriginalDataset/ testdata.csv 0.0
+python SDM.py ./Dataset/ ./OriginalDataset/ testdata.csv 0.05
+python SDM.py ./Dataset/ ./OriginalDataset/ testdata.csv 0.1
+python SDM.py ./Dataset/ ./OriginalDataset/ testdata.csv 0.15
+python SDM.py ./Dataset/ ./OriginalDataset/ testdata.csv 0.2
+python SDM.py ./Dataset/ ./OriginalDataset/ testdata.csv 0.25
+python SDM.py ./Dataset/ ./OriginalDataset/ testdata.csv 0.3
+
 
