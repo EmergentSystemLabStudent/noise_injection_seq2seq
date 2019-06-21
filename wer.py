5bc8fdf32ae2589a37170d0777699db1888ff60d
emlab <rokitaniplus1@gmail.com>
2019-06-21 12:22:10 +0900

    wer

--- wer.py
+++ wer.py
@@ -1,1 +1,13 @@
 from jiwer import wer
+import pandas as pd
+import sys
+if __name__ == '__main__':
+    dir = sys.argv[1]
+    df = pd.read_csv(dir+"SpeechRecognitionResults.csv")
+    for index,row in df.iterrows():
+        s1=row.original_sentence
+        s1=s1.replace("_"," ")
+        s1=s1.replace("."," ")
+        s2=row.google_word
+        print(index,wer(s1,s2))
+
---
+++ wer.sh
@@ -0,0 +1,4 @@
+python wer.py ./speech_recognition_results/speaker_K/ > ./speech_recognition_results/speaker_K/wer.txt
+python wer.py ./speech_recognition_results/speaker_L/ > ./speech_recognition_results/speaker_L/wer.txt
+python wer.py ./speech_recognition_results/speaker_T/ > ./speech_recognition_results/speaker_T/wer.txt
+python wer.py ./speech_recognition_results/speaker_W/ > ./speech_recognition_results/speaker_W/wer.txt 
