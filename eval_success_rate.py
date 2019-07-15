5f776e079cd280f860ace78480a35152d4346b23
emlab <rokitaniplus1@gmail.com>
2019-07-15 22:00:52 +0900

    eval success rate

---
+++ eval_success_rate.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import sys
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    list = pd.read_csv("GPSRsentence_list.csv")
+    label = list['label']
+    results = open(filename,"r")
+    result =results.readlines()
+    success = 0
+    for i in range(100):
+        #print(i)
+        #print(result[i])
+        #print(label[i])
+        if result[i] == label[i]+"\n":
+            #print("1")
+            success = success + 1
+        #else:
+            #print("0")
+        #print("\n")
+    results.close()
+    print(filename,success)
+    #for label in :
+    #    print(label)
+
+
