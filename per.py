623e71d780b35aa4f351dd352cc43a4cecc7302e
emlab <rokitaniplus1@gmail.com>
2019-07-24 15:23:09 +0900

    per

---
+++ per.py
@@ -0,0 +1,22 @@
+from jiwer import wer
+import pandas as pd
+import sys
+if __name__ == '__main__':
+    f=open("GPSRsentence_phoneme_list","r")
+    dir="./speech_recognition_results/speaker_K/"
+    g=open(dir+"google_phonemeresultdata.csv")
+    s=open(dir+"sphinx_phonemeresultdata.csv")
+    sumg=0
+    sums=0
+
+    for i in range(100):
+        print(i)
+        s1=f.readline()
+        s2=g.readline()
+        s3=s.readline()
+        wg=wer(s1,s2)
+        ws=wer(s1,s3)
+        print(i,wg,ws)
+        sumg+=wg
+        sums+=ws
+    print("avg",sumg/100.0,sums/100.0)
---
+++ per.sh
@@ -0,0 +1,4 @@
+python per.py ./speech_recognition_results/speaker_K/ > ./speech_recognition_results/speaker_K/per.txt
+python per.py ./speech_recognition_results/speaker_L/ > ./speech_recognition_results/speaker_L/per.txt
+python per.py ./speech_recognition_results/speaker_T/ > ./speech_recognition_results/speaker_T/per.txt
+python per.py ./speech_recognition_results/speaker_W/ > ./speech_recognition_results/speaker_W/per.txt 
