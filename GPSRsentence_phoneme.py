cc1f5dd2f5370bc7e252dbd6ebd23a9fb44a6383
emlab <rokitaniplus1@gmail.com>
2019-07-24 14:10:31 +0900

    GPSRsentence_phoneme

---
+++ GPSRsentence_phoneme.py
@@ -0,0 +1,14 @@
+from jiwer import wer
+import pandas as pd
+import sys
+import pronouncing
+if __name__ == '__main__':
+    df = pd.read_csv("./GPSRsentence_list.csv")
+    for index,row in df.iterrows():
+        phoneme = ""
+        for word in row.original_sentence.replace("_"," ").split(" "):
+            ph = pronouncing.phones_for_word(str(word))
+            if ph:
+                phoneme = phoneme + ph[0]+" "
+        print(index,phoneme)
+
