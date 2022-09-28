fc085095357f3db510ef18683c218a6e830a8fe0
Hiroki Tanaka <rokitaniplus1@gmail.com>
2022-09-28 10:32:35 +0900

    rename SDM -> inject_noise

--- inject_noise.py
+++ inject_noise.py
@@ -4,7 +4,6 @@ import pandas as pd
 import sys
 from tqdm import tqdm 
 import os
-import shutil
 
 phenome = ["AA",
 "AA0",
@@ -94,11 +93,11 @@ phenome = ["AA",
 d = {s: v for s,v in enumerate(phenome) }
 cmudict = {"Softner":"S AO F AX N AX"}
 
-def ReadFile(filename):
+def load_data(filename):
     df = pd.read_csv(filename,encoding="cp932")
-    datas = df["data"]
-    labels = df["labeldata"]
-    return datas,labels
+    data = df["data"]
+    label = df["labeldata"]
+    return data,label
 
 def random_phoneme():
     index = int(np.random.randint(0, len(phenome), 1))
@@ -117,15 +116,15 @@ def Substitution(ph,rho):
 
 def adjust_sentence(sentence):
     adj_sentence = []
-    for i, word in enumerate(sentence.split(" ")):
+    for _, word in enumerate(sentence.split(" ")):
         if "_" in word:
-            splitWord = word.split("_")
-            for data in splitWord:
-                adj_sentence.append(data)
+            splited_words = word.split("_")
+            for w in splited_words:
+                adj_sentence.append(w)
         elif "nearby" in word:
-            splitWord = ["near","by"]
-            for data in splitWord:
-                adj_sentence.append(data)
+            splited_words = ["near","by"]
+            for w in splited_words:
+                adj_sentence.append(w)
         elif "." in word:
             continue
         else:
@@ -134,7 +133,7 @@ def adjust_sentence(sentence):
 
 def get_phenome_Sentence(adj_sentence):
     phenomeSentence = []
-    for j,word in enumerate(adj_sentence):
+    for _,word in enumerate(adj_sentence):
         ph = pronouncing.phones_for_word(word)
         if not ph:
             print("--------------------",word,"--------------------")
@@ -147,7 +146,7 @@ def get_phenome_Sentence(adj_sentence):
 
 def edit_phenome_sentence(phenomeSentence,rho):
     edited_phenome_sentence=[]
-    for i, phletter in enumerate(phenomeSentence):
+    for _, phletter in enumerate(phenomeSentence):
         while True:
             if(np.random.binomial(1, rho)):
                 edited_phenome_sentence.append(Insert())
@@ -157,30 +156,26 @@ def edit_phenome_sentence(phenomeSentence,rho):
             edited_phenome_sentence.append(Substitution(phletter,rho))
     return edited_phenome_sentence
 
-if __name__ == '__main__':
-    #sys.argv[1]:edited data directory path
-    #sys.argv[2]:original data directory path
-    #sys.argv[3]:train/test/dev
-    #sys.argv[4]:noise level
-
+def main():
     edited_dir_path = sys.argv[1]
     original_dir_path = sys.argv[2]
     original_filename = sys.argv[3]
     rho=float(sys.argv[4])
     data_filepath = original_dir_path + original_filename +"data.csv" 
 
-    sentences,labels = ReadFile(data_filepath)
+    sentences,labels = load_data(data_filepath)
 
     str_noiselevel = "rho="+str(rho)
     print(str_noiselevel)
-    phenomedatas = []
-    noisydatas = []
+    
+    phenome_data = []
+    noisy_data = []
     for j,sentence in enumerate(tqdm(sentences)):
         adj_sentence = adjust_sentence(sentence)
         phenomeSentence = get_phenome_Sentence(adj_sentence)
-        phenomedatas.append(phenomeSentence)
+        phenome_data.append(phenomeSentence)
         edited_phenome_sentence = edit_phenome_sentence(phenomeSentence,rho)
-        noisydatas.append(edited_phenome_sentence)
+        noisy_data.append(edited_phenome_sentence)
 
     os.mkdir(edited_dir_path+str_noiselevel)
     with open(edited_dir_path+str_noiselevel+"/"+"original_sentence_"+original_filename.replace(".csv","")+".in",mode="w") as f:
@@ -189,12 +184,12 @@ if __name__ == '__main__':
             f.write("\n")
 
     with open(edited_dir_path+str_noiselevel+"/"+"phoneme_"+original_filename.replace(".csv","")+".in",mode="w") as f:
-        for i, phenomeSentence in enumerate(phenomedatas):
+        for i, phenomeSentence in enumerate(phenome_data):
             f.write(" ".join(phenomeSentence))
             f.write("\n")
 
     with open(edited_dir_path+str_noiselevel+"/"+"edited_phoneme_"+original_filename.replace(".csv","")+".in",mode="w") as f:
-        for i, noisySentence in enumerate(noisydatas):
+        for i, noisySentence in enumerate(noisy_data):
             f.write(" ".join(noisySentence))
             f.write("\n")
 
@@ -206,6 +201,15 @@ if __name__ == '__main__':
     with open(edited_dir_path+str_noiselevel+"/"+original_filename,mode="w") as f:
         f.writelines("original_sentence,phoneme,edited_phoneme,label\n")
         for i, (train_data, phenomeSentence, noisySentence, labelSentence) in enumerate(
-                zip(sentences, phenomedatas, noisydatas, labels)):
+                zip(sentences, phenome_data, noisy_data, labels)):
             f.write(train_data+","+" ".join(phenomeSentence)+","+" ".join(noisySentence)+","+labelSentence)
             f.write("\n")
+
+if __name__ == '__main__':
+    main()
+    #sys.argv[1]:edited data directory path
+    #sys.argv[2]:original data directory path
+    #sys.argv[3]:train/test/dev
+    #sys.argv[4]:noise level
+
+    
---
+++ inject_noise.sh
@@ -0,0 +1,11 @@
+python inject_noise.py ./learning_dataset/ ./original_dataset/ train 0.05
+python inject_noise.py ./learning_dataset/ ./original_dataset/ train 0.10
+python inject_noise.py ./learning_dataset/ ./original_dataset/ train 0.15
+python inject_noise.py ./learning_dataset/ ./original_dataset/ train 0.20
+python inject_noise.py ./learning_dataset/ ./original_dataset/ train 0.25
+python inject_noise.py ./learning_dataset/ ./original_dataset/ train 0.30
+
+
+
+
+
